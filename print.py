7fdbad90cee1b7a4b829abcc5388b4a529a406e5
= <=>
2024-04-02 06:08:30 -0700

    print stmts

---
+++ print.py
@@ -0,0 +1,7 @@
+rollno=1122
+name="Vishnu"
+print("Roll no: %d Name: %s" %(rollno,name )) #Print stmt1 using %(modulo)
+print("Rollno: {},name: {}".format( name,rollno)) #Print stmt2 >>using format()
+print("Rollno: {0},Name:{1}".format(rollno,name)) #print stmt2 >>using index 
+print("{} Rollno is {}".format(name,rollno)) #print stmt2 using format()
+print(f"{name}Rollno is {rollno}") #print stmt3 using f-string
