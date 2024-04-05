cdc3f3583f1c526518e671860a7ce8a6dc58e89d
= <=>
2024-04-05 04:40:51 -0700

    Aruments & parameters

---
+++ arguments.py
@@ -0,0 +1,16 @@
+def add(name,rno):
+    print(f"{name} roll no is {rno}" )
+add("Vishnu",1122)    #Keyword only  
+add(1122,"Vishnu")
+add(rno=1122,name="Priya") #position only
+add(name="Priya",rno=1122)
+ 
+def sum(x,y=5): #Default parameter y=0 or y=5 
+     print("sum=",x+y)
+sum(10)      
+
+def show(*x): #arbitary arguments *arg
+    print(x)
+show(10)
+show(10,20)
+show(10,20,30,40,50,60,70,80,90,100)
