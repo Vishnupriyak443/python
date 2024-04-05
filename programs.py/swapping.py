fe997710d609ecd2302ae24f1a6bfd41f879f4b7
= <=>
2024-04-05 04:51:43 -0700

    Swapping

---
+++ programs.py/swapping.py
@@ -0,0 +1,9 @@
+print("Before swapping:")
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+x=x+y
+y=x-y
+x=x-y
+print("After swapping:")
+print("First number:",x)
+print("Second number:",y)
