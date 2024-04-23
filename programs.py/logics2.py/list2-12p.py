ab10b65be43fa0b211529e36d33e0637e63e7022
= <=>
2024-04-23 04:13:26 -0700

    Replacing 20-200

--- programs.py/logics2.py/list2-12p.py
+++
@@ -1,5 +0,0 @@
-list=[10,20,30,40,20,30,60,50]
-for i in list:
-    if(i==20):
-        i=200
-print(list)        m
---
+++ programs.py/logics2.py/replace20-200.py
@@ -0,0 +1,4 @@
+list=[10,20,30,40,20,30,60,50]
+i=list.index(20)
+list[i]=200
+print(list)
