5748e7bb91177b95757cfa7c3055f4a3ae023e14
= <=>
2024-04-24 05:24:44 -0700

    Counting occurance of an item in a tuple

--- programs.py/tuple.py/tuple-6.py
+++ programs.py/tuple.py/tuple-6.py
@@ -1,7 +1,3 @@
 #Counting no.of occuraces of an item from a tuple
 tup=(10,20,50,30,50,60,70,50)
-count=0
-for i in tup:
-    if (i==50):
-        count=count+1
-print(count)        
+print(tup.count(50))
