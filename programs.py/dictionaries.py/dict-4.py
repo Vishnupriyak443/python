a75c9563959a86a694ce9b6b11ce5bf946c486ad
= <=>
2024-04-25 04:34:48 -0700

    Removing list of keys in dictionary

--- programs.py/dictionaries.py/dict-4.py
+++ programs.py/dictionaries.py/dict-4.py
@@ -1,4 +1,4 @@
 sampledict={"name":"ABC","age":25,"salary":8000,"city":"Nellore"}
-del sampledict["age"]
-sampledict.pop("city")
+del sampledict["name"]
+sampledict.pop("salary")
 print(sampledict)
---
+++ programs.py/dictionaries.py/dict-5.py
@@ -0,0 +1,5 @@
+dict={"name":"ABC","age":25,"salary":8000,"city":"Nellore"}
+x="ABC"
+for i in dict:
+    if(i==x):
+        print("found")
