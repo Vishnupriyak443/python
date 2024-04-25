ec7b6d9b8c6389ddd9fb0cb10bc6f4c3a3ed61fe
= <=>
2024-04-25 05:52:03 -0700

    Checking value exist in dictionary

--- programs.py/dictionaries.py/dict-5.py
+++ programs.py/dictionaries.py/dict-5.py
@@ -1,6 +1,5 @@
 dict={"name":"ABC","age":25,"salary":8000,"city":"Nellore"}
-print(dict["name"])
-value="name"
-for i in dict:
-    if(i==value):
-        print(value,"found")
+if "ABC" in dict.values():
+    print("exists")
+if 8000 in dict.values():
+    print("exists")
