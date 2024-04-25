b90d43d8aaad01e4ab215ebbdaeb997f8f8d4ef5
= <=>
2024-04-25 05:36:12 -0700

    Rename key of a dict

---
+++ programs.py/dictionaries.py/dict-6.py
@@ -0,0 +1,4 @@
+dict={"name":"ABC","age":25,"salary":8000,"city":"Nellore"}
+dict.pop("city")
+dict.update({"location":"Nellore"})
+print(dict)
---
+++ programs.py/dictionaries.py/dict-7.py
@@ -0,0 +1,2 @@
+sampledict={'emp1':{'name':'pawan','salary':7500},'emp2':{'name':'priya','salary':8000},'emp3':{'name':'raj','salary':500}}
+sampledict['emp3']['salary']
