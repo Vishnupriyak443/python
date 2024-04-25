331b0190ef253e1944e3048f48d4bb386c739c13
= <=>
2024-04-25 05:44:24 -0700

    CHANGING VALUE OF A KEY IN A DICTIONARY

--- programs.py/dictionaries.py/dict-7.py
+++ programs.py/dictionaries.py/dict-7.py
@@ -1,2 +1,5 @@
 sampledict={'emp1':{'name':'pawan','salary':7500},'emp2':{'name':'priya','salary':8000},'emp3':{'name':'raj','salary':500}}
-sampledict['emp3']['salary']
+del sampledict['emp3']['salary']
+print(sampledict)
+sampledict['emp3']['salary']=8500
+print(sampledict)
